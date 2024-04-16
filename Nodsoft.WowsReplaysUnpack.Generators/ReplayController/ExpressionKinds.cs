270ba78e2e5f46d6c94ee8d411ed4959db2133ca
stewieoO <developer@stewieoo.com>
2024-04-16 23:51:19 +0200

    remove unused class

--- Nodsoft.WowsReplaysUnpack.Generators/ReplayController/ExpressionKinds.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Nodsoft.WowsReplaysUnpack.Generators.ReplayController;
-
-public static class ExpressionKinds
-{
-	public const int TrueLiteral = 8752;
-	public const int Unary = 8731;
-	public const int MinusUnary = 8749;
-}
