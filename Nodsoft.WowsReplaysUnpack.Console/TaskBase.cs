560133226cec635efb460e0b032470bc6c75f167
stewieoO <developer@stewieoo.com>
2024-04-16 22:58:14 +0200

    cleanup console project

---
+++ Nodsoft.WowsReplaysUnpack.Console/TaskBase.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Nodsoft.WowsReplaysUnpack.Console;
+
+public abstract class TaskBase
+{
+	private static readonly string _samplePath = Path.Join(Directory.GetCurrentDirectory(),
+		"../../../../Nodsoft.WowsReplaysUnpack.Tests",
+		"Replay-Samples");
+
+	protected FileStream GetReplayFile(string name) =>
+		File.Open(Path.Join(_samplePath, name), FileMode.Open, FileAccess.Read, FileShare.Read);
+
+	protected abstract void ConfigureServices(IServiceCollection serviceCollection);
+
+	public async Task ExecuteAsync()
+	{
+		ServiceCollection serviceCollection = new();
+		serviceCollection.AddLogging(logging =>
+		{
+			logging.ClearProviders();
+			logging.AddConsole();
+			logging.SetMinimumLevel(LogLevel.Information);
+		});
+
+		ConfigureServices(serviceCollection);
+
+		ServiceProvider services = serviceCollection.BuildServiceProvider();
+
+		await ExecuteAsync(services);
+	}
+
+	protected abstract Task ExecuteAsync(IServiceProvider services);
+}
---
+++ Nodsoft.WowsReplaysUnpack.Console/Tests/SyncTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Nodsoft.WowsReplaysUnpack.Core.Models;
+using Nodsoft.WowsReplaysUnpack.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nodsoft.WowsReplaysUnpack.Console.Tests;
+
+public class SyncTest : TaskBase
+{
+	protected override void ConfigureServices(IServiceCollection serviceCollection)
+	{
+		serviceCollection.AddWowsReplayUnpacker();
+	}
+
+	protected override Task ExecuteAsync(IServiceProvider services)
+	{
+		using IReplayUnpackerService<UnpackedReplay> unpacker =
+			services.GetRequiredService<IReplayUnpackerFactory>().GetUnpacker();
+
+		_ = RunTasks(unpacker, 20, false);
+
+		return Task.CompletedTask;
+	}
+
+	private UnpackedReplay[] RunTasks(IReplayUnpackerService<UnpackedReplay> unpacker, int cycles,
+		bool sync)
+	{
+		List<UnpackedReplay> unpackedReplays = new();
+		if (sync)
+		{
+			for (int i = 0; i < cycles; i++)
+			{
+				unpacker.Unpack(GetReplayFile("good.wowsreplay"));
+			}
+		}
+		else
+		{
+			Parallel.ForEach(Enumerable.Range(0, cycles), _ =>
+			{
+				unpackedReplays.Add(unpacker.Unpack(GetReplayFile("good.wowsreplay")));
+			});
+		}
+
+		return [..unpackedReplays];
+	}
+}
