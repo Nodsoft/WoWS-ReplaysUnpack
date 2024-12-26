c25163a87cf44578ef3304084fc46b0e9b714523
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2024-12-26 16:16:27 +0100

    fix(core): handle nullable vector in data reading
    - Added a check for nullable vectors to return null when appropriate.
    - Refactored the reading of float values into a loop for clarity.
    - Improved readability by using an array instead of LINQ for float value collection.

--- Nodsoft.WowsReplaysUnpack.Core/DataTypes/Vector/VectorDataType.cs
+++ Nodsoft.WowsReplaysUnpack.Core/DataTypes/Vector/VectorDataType.cs
@@ -20,9 +20,21 @@ internal class VectorDataType : DataTypeBase
 	/// </summary>
 	protected override object? GetValueInternal(BinaryReader reader, XmlNode? propertyOrArgumentNode, int headerSize)
 	{
+		// HACK: Check for nullable vector (NULLABLE_VECTOR{n})
+		if (headerSize is 1 && propertyOrArgumentNode is { InnerText: { } type } && type.StartsWith("NULLABLE_VECTOR"))
+		{
+			return null;
+		}
+		
 		// Size of a float value
-		return Enumerable.Range(0, _itemCount)
-			.Select(_ => reader.ReadSingle()).ToArray();
+		float[] result = new float[_itemCount];
+		
+		for (int i = 0; i < _itemCount; i++)
+		{
+		    result[i] = reader.ReadSingle();
+		}
+		
+		return result.ToArray();
 	}
 
 	public override object? GetDefaultValue(XmlNode? propertyOrArgumentNode, bool forArray = false)
